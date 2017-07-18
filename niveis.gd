3de323392ed94231444e7a4943329426ebc91c9c
tananmurilo <tananmurilo@gmail.com>
2017-07-18 20:08:06 -0300

    Algumas imagens

--- niveis.gd
+++ niveis.gd
@@ -1,8 +1,7 @@
 extends Control
 
 func _ready():
-	get_node("Button 4").connect("pressed",self,"_on_Button_pressed")
-	get_node("Button").connect("pressed",self,"fase1")
+	get_node("Fase1").connect("pressed",self,"fase1")
 
 func _on_Button_pressed():
 	get_node("/root/global").goto_scene("res://mainmenu.tscn")
